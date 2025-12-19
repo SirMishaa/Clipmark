e9c977f2f789eb800bacffe061f01e68315940a5
Mishaa <mishaa.pro@proton.me>
2025-12-19 14:55:16 +0100

    fix: change assertStatus to 302 (redirect) for the dashboard page test
    will fix CI

--- tests/Feature/DashboardTest.php
+++ tests/Feature/DashboardTest.php
@@ -16,12 +16,12 @@ class DashboardTest extends TestCase
         $response->assertRedirect(route('login'));
     }
 
-    public function test_authenticated_users_can_visit_the_dashboard()
+    public function test_authenticated_users_can_visit_the_dashboard_and_be_redirect()
     {
         $user = User::factory()->create();
         $this->actingAs($user);
 
         $response = $this->get(route('dashboard'));
-        $response->assertStatus(200);
+        $response->assertStatus(302);
     }
 }
